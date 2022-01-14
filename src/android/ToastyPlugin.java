89d6cd6269722f1624b03dfc79815809358628e7
DrTardis1 <techbryce@hotmail.com>
2022-01-14 17:27:26 +1100

    Updated filename to be ToastyPlugin

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -22,7 +22,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
-public class MockLocationChecker extends CordovaPlugin{
+public class ToastyPlugin extends CordovaPlugin{
 
     private int MY_PERMISSIONS_REQUEST = 0;
 
