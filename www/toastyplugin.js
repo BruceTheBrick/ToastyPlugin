8a4478d4c4ff00b04408c5fe08bb98dddc009f05
DrTardis1 <techbryce@hotmail.com>
2022-01-14 15:22:42 +1100

    Tried modifying toastyplugin.js

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -1,19 +1,45 @@
-function ToastyPlugin() {}
+// function ToastyPlugin() {}
 
-ToastyPlugin.prototype.show = function (message, duration, successCallback, errorCallback) {
-  var options = {};
-  options.message = message;
-  options.duration = duration;
+// ToastyPlugin.prototype.show = function (message, duration, successCallback, errorCallback) {
+//   var options = {};
+//   options.message = message;
+//   options.duration = duration;
 
-  cordova.exec(successCallback, errorCallback, "ToastyPlugin", "show", [options]);
+//   cordova.exec(successCallback, errorCallback, "ToastyPlugin", "show", [options]);
+// };
+
+// function show (message, duration, successCallback, errorCallback) {
+//   var options = {};
+//   options.message = message;
+//   options.duration = duration;
+
+//   cordova.exec(successCallback, errorCallback, "ToastyPlugin", "show", [options]);
+// }
+
+var ToastyPlugin = {
+  show: function (message, duration, successCallback, errorCallback) {
+    var options = {};
+    options.message = message;
+    options.duration = duration;
+
+    cordova.exec(successCallback, errorCallback, "ToastyPlugin", "show", [options]);
+  },
 };
 
-ToastyPlugin.install = function () {
+// ToastyPlugin.install = function () {
+//   if (!window.plugins) {
+//     window.plugins = {};
+//   }
+//   window.plugins.toastyPlugin = new ToastyPlugin();
+//   return window.plugins.toastyPlugin;
+// };
+
+// cordova.addConstructor(ToastyPlugin.install);
+
+cordova.addConstructor(function () {
   if (!window.plugins) {
     window.plugins = {};
   }
-  window.plugins.toastyPlugin = new ToastyPlugin();
+  window.plugins.toastyPlugin = ToastyPlugin;
   return window.plugins.toastyPlugin;
-};
-
-cordova.addConstructor(ToastyPlugin.install);
+});
