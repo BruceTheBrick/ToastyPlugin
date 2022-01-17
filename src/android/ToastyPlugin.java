c50a2fb6975b359186f98d0e31c226d59c39d377
DrTardis1 <techbryce@hotmail.com>
2022-01-17 18:13:22 +1100

    Tried newer versions of isMock()

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -33,47 +33,33 @@ public class ToastyPlugin extends CordovaPlugin{
     boolean isNetworkEnabled = false;
     private String LOCATION_PROVIDER = "";
     LocationListener locationListener;
-    LocationManager locationManager;
     private boolean listenerON = false;
     private String statusMock = "";
     private JSONArray arrayGPS = new JSONArray();
     private JSONObject objGPS = new JSONObject();
-    private Location GLOBAL_LOCATION;
 
     @Override
     public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
+
         if (action.equals("show")) {
           LocationManager locationManager = (LocationManager) this.cordova.getActivity().getSystemService(Context.LOCATION_SERVICE);
-          // LocationProvider lProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
           Location myloc = new Location(LocationManager.GPS_PROVIDER);
-          boolean isSpoofed = tempIsMock() ? true : false;
-          try{
-            objGPS.put("isMock", isSpoofed);
-            objGPS.put("tempMock", tempIsMock());
-          }catch(JSONException e){
-            return false;
-          }
-          locationListener = new LocationListener(){
-            public void onLocationChanged(Location location){
-
-              try{
-              objGPS.put("newTest", location.isFromMockProvider());
-              }catch(JSONException e){
-                return;
-              }
-              callbackContext.success(objGPS);
-            }
-          };
-          // callbackContext.success(objGPS);
+          boolean isSpoofed = myloc.isMock() ? true : false;
+          objGPS.put("isMock", isSpoofed);
+          objGPS.put("location", myloc);
+          callbackContext.success(objGPS);
           return true;
         }
+
+
         else{
-          return false; 
+          return false;
         }
+        // }
+        
+        // else {
+        //   return false;
+        // }
 
-    }
-
-    public boolean tempIsMock(){
-      return Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0");
     }
 }
