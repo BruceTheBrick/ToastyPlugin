dd1eefe1eee16c869f908348227579bfb60e1cc8
DrTardis1 <techbryce@hotmail.com>
2022-01-20 14:09:04 +1100

    ..

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -19,7 +19,7 @@ import javax.security.auth.callback.Callback;
 public class ToastyPlugin extends CordovaPlugin{
 
   private JSONObject objGPS = new JSONObject();
-  private String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_SETTINGS};
+  private String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
   private CallbackContext context;
   private Context ctx;
 
@@ -30,21 +30,10 @@ public class ToastyPlugin extends CordovaPlugin{
 
       if (action.equals("show")) {
           getPerms(1);
-          disableMocking();
           checkDevOptions();
-          checkPackageName();
-
-          objGPS.put("hasPerms", hasPerms());
-          
+          checkPackageName();      
           callbackContext.success(objGPS);
           return true;
-          // Context ctx = this.cordova.getActivity().getApplicationContext();
-          // LocationManager locationManager = (LocationManager) this.cordova.getActivity().getSystemService(Context.LOCATION_SERVICE);
-          // List<String> providers = locationManager.getAllProviders();
-          // Location myloc = new Location(LocationManager.GPS_PROVIDER);
-          // boolean isSpoofed = myloc.isFromMockProvider() ? true : false;
-          // objGPS.put("isMock", isSpoofed);
-          // objGPS.put("hasPerms", hasPerms());
         }
         else{
           return false;
@@ -52,26 +41,9 @@ public class ToastyPlugin extends CordovaPlugin{
 
     }
 
-    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException{
-      PluginResult result;
-
-      if(context != null){
-        for(int r : grantResults){
-          if(r == PackageManager.PERMISSION_DENIED){
-            result = new PluginResult(PluginResult.Status.ILLEGAL_ACCESS_EXCEPTION);
-            // context.sendPluginResult(result);
-            return;
-          }
-        }
-        result = new PluginResult(PluginResult.Status.OK);
-        // context.sendPluginResult(result);
-      }
-    }
-
     private boolean hasPerms() throws JSONException{
       boolean hasPerms = true;
       for(String p : permissions){
-        objGPS.put(p, PermissionHelper.hasPermission(this, p));
         if(!PermissionHelper.hasPermission(this, p)) hasPerms = false;
       }
       return hasPerms;
@@ -79,45 +51,6 @@ public class ToastyPlugin extends CordovaPlugin{
 
     private void getPerms(int requestCode){
       PermissionHelper.requestPermissions(this, requestCode, permissions);
-
-      silentEnableMockPerms();
-    }
-
-    private boolean disableMocking(){
-      LocationManager lm = (LocationManager) this.cordova.getActivity().getSystemService(Context.LOCATION_SERVICE);
-      List<String> providers = lm.getAllProviders();
-      try{
-        for(String provider : providers){
-          lm.removeTestProvider(provider);
-          objGPS.put("removedTestProviders", true);
-        }
-        return true;
-      }catch(Exception e){
-        try{
-          objGPS.put("removedTestProviders", false);
-          objGPS.put("error", e.toString());
-        }catch(Exception ee){
-          return false;
-        }
-        return false;
-      }
-    }
-
-    private boolean silentEnableMockPerms(){
-      boolean success = false;
-      try{
-        Settings.Secure.putString(ctx.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION, "1");
-        objGPS.put("mockEnable", true);
-        success = true;
-      }catch(Exception e){
-        try{
-          objGPS.put("mockEnable", e.toString());
-          success = false;
-        }catch(Exception ee){
-          success = false;
-        }
-      }
-      return success;
     }
 
     private void checkDevOptions(){
@@ -130,7 +63,6 @@ public class ToastyPlugin extends CordovaPlugin{
         e.printStackTrace();
       }
     }
-
 
     private void checkPackageName() throws JSONException{
       String currName = ctx.getPackageName();
